138e6dcf9ba75daadaae656ff3f6a5891dfd8dac
Arxoto <1484663586@qq.com>
2025-03-09 23:36:45 +0800

    init and add course_0

---
+++ src/main.rs
@@ -0,0 +1,112 @@
+/// # course_0: Build project
+/// 
+/// see [bevy-quick-start](https://bevyengine.org/learn/quick-start/getting-started/setup/)
+/// 
+/// ## Create project in Windows
+/// 
+/// ```powershell
+/// cargo new hello_bevy
+/// cd hello_bevy
+/// cargo add bevy
+/// ```
+/// 
+/// ## 多项目配置 If using Cargo Workspaces (Optional)
+/// 
+/// edit Cargo.toml
+/// 
+/// ```toml
+/// [workspace]
+/// resolver = "2" # Important! wgpu/Bevy needs this!
+/// ```
+/// 
+/// ## 优化编译性能 Compile with Performance Optimizations
+/// 
+/// edit Cargo.toml
+/// 
+/// ```toml
+/// # Enable a small amount of optimization in the dev profile.
+/// [profile.dev]
+/// opt-level = 1
+/// 
+/// # Enable a large amount of optimization in the dev profile for dependencies.
+/// [profile.dev.package."*"]
+/// opt-level = 3
+/// ```
+/// 
+/// ## 发布模式优化 Release Mode Optimizations (Optional)
+/// 
+/// edit Cargo.toml
+/// 
+/// ```toml
+/// # Enable more optimization in the release profile at the cost of compile time.
+/// [profile.release]
+/// # Compile the entire crate as one unit.
+/// # Slows compile times, marginal improvements.
+/// codegen-units = 1
+/// # Do a second optimization pass over the entire program, including dependencies.
+/// # Slows compile times, marginal improvements.
+/// lto = "thin"
+/// ```
+/// 
+/// P.S. 还有个 wasm-release 模式的配置没写 因为不涉及该项目 可参考官网进行配置
+/// 
+/// ## 启动快速编译 Enable Fast Compiles (Optional)
+/// 
+/// ### 动态连接 Dynamic Linking
+/// 
+/// ```powershell
+/// cargo add bevy -F dynamic_linking
+/// ```
+/// 
+/// ### 替代链接器 Alternative Linkers (LLD)
+/// 
+/// ```powershell
+/// rustup update # update toolchains
+/// cargo install -f cargo-binutils
+/// rustup component add llvm-tools-preview
+/// ```
+/// 
+/// edit .cargo/config.toml
+/// 
+/// ```toml
+/// # for Windows
+/// [target.x86_64-pc-windows-msvc]
+/// linker = "rust-lld.exe"
+/// ```
+/// 
+/// ## 提高运行时性能 Improve Runtime Performance (Optional)
+/// 
+/// edit Cargo.toml
+/// 
+/// ```toml
+/// [dependencies]
+/// log = { version = "*", features = ["max_level_debug", "release_max_level_warn"] }
+/// ```
+/// 
+/// ## How to run
+/// 
+/// edit .vscode/launch.json "configurations"
+/// 
+/// ```json
+/// {
+///     "command": "cargo run",
+///     "name": "Cargo Run",
+///     "request": "launch",
+///     "cwd": "${workspaceFolder}/target/debug",
+///     "type": "node-terminal"
+/// }
+/// ```
+/// 
+/// ## tips
+/// 
+/// - see course doc
+///   - run `cargo doc --open`
+/// - update bevy version
+///   - edit the version in Cargo.toml "dependencies"
+///   - run `cargo update`
+/// - add subproject
+///   - run `cargo new subproject --lib`
+///   - edit Cargo.toml "dependencies" `subproject = { path = "./subproject" }`
+fn main() {
+    println!("Hello, world!");
+}
